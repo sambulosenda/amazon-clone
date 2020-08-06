02abdf1f7857a3837fe909be1dad138891301edf
Sambulo Senda <sambulosendas@gmail.com>
2020-08-06 17:12:06 +0100

    first commit

---
+++ src/Header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Header.css';
+import {Link} from "react-router-dom";
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header() {
+    return (
+        <div className="header">
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon clone"/>
+            </Link>
+
+            {/* {Search } */}
+            <div className="header__search">
+                <input type="text" className="header__searchInput"/>
+                <SearchIcon className="header__searchIcon"/>
+            </div>
+
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                    <div className="header_option">
+                        <span class="header_optionLineOne">Hello,</span>
+                        <span class="header_optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                    <div className="header_option">
+                        <span class="header_optionLineOne">Returns</span>
+                        <span class="header_optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                    <div className="header_option">
+                        <span class="header_optionLineOne">Your</span>
+                        <span class="header_optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+
+
+                {/*  */}
+                <Link to="/checkout" className="header-link">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon/>
+                        <span className="header__optionLineTwo busketcount">0</span>
+                    </div>
+                </Link>
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Header
